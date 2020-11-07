adf43d37bda874496d1fd39f8bf1a026c975f3ed
Stacy Potten <stacylynnaz@gmail.com>
2020-11-07 11:51:55 -0700

    import router

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -5,7 +5,7 @@ const Card = (props) => {
   return (
     <div id={props.id} className='plant-card'>
       <nav className='card-info'>
-        <img className='plant-photo' src={props.image} alt= {props.commonName} photo/>
+        <img className='plant-photo' src={props.image} alt= {props.commonName}/>
         <h2>{props.commonName}</h2>
         <h2 className='scientific-name'>{props.scientificName}</h2>
       </nav>
