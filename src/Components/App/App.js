af2cf45d2f0a31aff8546492578d14039f9b4ebf
Stacy Potten <stacylynnaz@gmail.com>
2020-11-07 12:04:55 -0700

    Add Link for Card Detail page

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CardContainer from '../CardContainer/CardContainer';
+import { Route, Link, withRouter } from 'react-router-dom';
 import { getPlants } from '../../apiCalls.js';
 import './App.css';
 
@@ -21,7 +22,14 @@ class App extends Component {
     return (
       <main>
         <h1 className='title'>Sakura</h1>
-        <CardContainer plantList={this.state.plants}/>
+        <Route exact path="/" render={() => <CardContainer plantList={this.state.plants}/>} />
+        <Route path='/plant/:id'
+          render={({ match }) =>{
+            const { id } = match.params;
+            const plantToRender = this.state.plants.find(plant => plant.id === parseInt(id));
+            return <CardDetail />
+          }}
+        />
       </main>
     )
   }
--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Card.css';
 
 const Card = (props) => {
   return (
     <div id={props.id} className='plant-card'>
       <nav className='card-info'>
-        <img className='plant-photo' src={props.image} alt= {props.commonName}/>
-        <h2>{props.commonName}</h2>
-        <h2 className='scientific-name'>{props.scientificName}</h2>
+        <Link to={'/plant/' + props.id}>
+          <img className='plant-photo' src={props.image} alt= {props.commonName}/>
+          <h2>{props.commonName}</h2>
+          <h2 className='scientific-name'>{props.scientificName}</h2>
+        </Link>
       </nav>
     </div>
   )
