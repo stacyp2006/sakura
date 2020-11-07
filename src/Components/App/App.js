1c2f7f62fb00440334065e548eaf7401f277e6d7
Stacy Potten <stacylynnaz@gmail.com>
2020-11-06 20:12:48 -0700

    Add render method

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import CardContainer from './CardContainer/CardContainer';
+// import CardContainer from '../CardContainer/CardContainer';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      
+
     }
+  }
+
+  render() {
+    return (
+      <main>
+        <h1>Sakura</h1>
+      </main>
+    )
   }
 }
 
