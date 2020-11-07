acd71401bbaf93e4a3c14f9a7286cc75b5d9635a
Stacy Potten <stacylynnaz@gmail.com>
2020-11-06 21:18:12 -0700

    Create CardContainer component with card return method

--- src/Components/CardContainer/CardContainer.js
+++ src/Components/CardContainer/CardContainer.js
@@ -1,4 +1,22 @@
 import React from 'react';
+import Card from '../Card/Card';
 import './CardContainer.css';
+
+const CardContainer = ({ plantList }) => {
+  let plantCards;
+  if(plantList.length !== 0) {
+    plantCards = plantList.map(plant => {
+      return (
+        <Card
+          id={plant.id}
+          key={plant.id}
+          commonName={plant.common_name}
+          scientificName={plant.scientific_name}
+          image={plant.image_url}
+        />
+      )
+    })
+  }
+}
 
 export default CardContainer;
