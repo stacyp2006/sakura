6cb99823473de26442baa41cc3d6680808dd79cd
Stacy Potten <stacylynnaz@gmail.com>
2020-11-08 13:31:25 -0700

    Add height to state and render

--- src/Components/CardDetail/CardDetail.js
+++ src/Components/CardDetail/CardDetail.js
@@ -8,12 +8,13 @@ class CardDetail extends Component {
     this.state = {
       plantType: {},
       plantInfo: [],
+      height: {}
     }
   }
 
   componentDidMount = () => {
     getSinglePlant(this.props.id)
-    .then(data => this.setState({plantType: data.data.specifications, plantInfo: data.data.sources[0]}))
+    .then(data => this.setState({plantType: data.data.specifications, plantInfo: data.data.sources[0], height: data.data.specifications.average_height}))
     .catch(error => console.log('single plant fetch error'))
   }
 
@@ -24,6 +25,8 @@ class CardDetail extends Component {
           <h1 className='common-name'>{this.props.common_name}</h1>
           <h2 className='scientific-name'>{this.props.scientific_name}</h2>
           {this.state.plantType.growth_habit !== null && <h2>Plant Type: {this.state.plantType.growth_habit}</h2>}
+          {this.state.height.cm  !== null &&
+          <h2>Height: {this.state.height.cm}cm</h2>}
           <a
           className='plant-link'
           href={this.state.plantInfo.url}
