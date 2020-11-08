b0e4e9e6dc0a80efff13e541ce62334b87a4e5c6
Stacy Potten <stacylynnaz@gmail.com>
2020-11-08 14:38:40 -0700

    Add conditional rendering to buttons.

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Card.css';
 
-const Card = (props) => {
-  return (
-    <div id={props.id} className='plant-card'>
-      <nav className='card-info'>
-        <Link to={'/plant/' + props.id}>
-          <img className='plant-photo' src={props.image} alt= {props.commonName}/>
-          <h2>{props.commonName}</h2>
-          <h2 className='scientific-name'>{props.scientificName}</h2>
-        </Link>
-      </nav>
-      <button className='add-button' type='button'>Add to Garden Plan</button>
-      <button className='remove-button' type='button'>Remove from Garden Plan</button>
-    </div>
-  )
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      onPlan: false,
+      quantity: ''
+    }
+  }
+
+  render() {
+    return (
+      <div id={this.props.id} className='plant-card'>
+        <nav className='card-info'>
+          <Link to={'/plant/' + this.props.id}>
+            <img className='plant-photo' src={this.props.image} alt= {this.props.commonName}/>
+            <h2>{this.props.commonName}</h2>
+            <h2 className='scientific-name'>{this.props.scientificName}</h2>
+          </Link>
+        </nav>
+        {!this.state.onPlan && <button className='add-button' type='button'>Add to Garden Plan</button>}
+        {this.state.onPlan && <button className='remove-button' type='button'>Remove from Garden Plan</button>}
+      </div>
+    )
+  }
 }
 
 //Add buttons when ready for the functionality
