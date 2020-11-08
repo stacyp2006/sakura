969a137583f471b5db28807d6c29b68a5f98c85e
Stacy Potten <stacylynnaz@gmail.com>
2020-11-08 13:22:32 -0700

    Add link for plantInfo

--- src/Components/CardDetail/CardDetail.js
+++ src/Components/CardDetail/CardDetail.js
@@ -6,25 +6,32 @@ class CardDetail extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      plantType: {}
+      plantType: {},
+      plantInfo: [],
     }
   }
 
   componentDidMount = () => {
     getSinglePlant(this.props.id)
-    .then(data => this.setState({plantType: data.data.specifications}))
+    .then(data => this.setState({plantType: data.data.specifications, plantInfo: data.data.sources[0]}))
     .catch(error => console.log('single plant fetch error'))
   }
 
   render() {
     return (
-      <main>
-        <h1>{this.props.common_name}</h1>
-        <h2>{this.props.scientific_name}</h2>
-        <h2>Native to Japan: </h2>
-        {this.state.plantType.growth_habit !== null && <h2>Plant Type: {this.state.plantType.growth_habit}</h2>}
-        <h2>Learn More: </h2>
-        <button className='add-button' type='button'>Add to Garden Plan</button>
+      <main className='plant-detail'>
+        <section className='plant-info'>
+          <h1 className='common-name'>{this.props.common_name}</h1>
+          <h2 className='scientific-name'>{this.props.scientific_name}</h2>
+          {this.state.plantType.growth_habit !== null && <h2>Plant Type: {this.state.plantType.growth_habit}</h2>}
+          <a
+          className='plant-link'
+          href={this.state.plantInfo.url}
+          target='_blank'>
+          Learn More
+          </a>
+          <button className='add-button' type='button'>Add to Garden Plan</button>
+        </section>
         <img className='photo' src={this.props.image_url} alt= {this.props.common_name}/>
       </main>
     )
