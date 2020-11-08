0c68123120a94923e23f7aa293bcde33a0909919
Stacy Potten <stacylynnaz@gmail.com>
2020-11-08 11:00:38 -0700

    Refactor to add async/await.

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -9,11 +9,12 @@ export const getPlants = () => {
   .then(response => response.json())
 }
 
-export const getSinglePlant = (plantID) => {
-  return fetch('https://fe-cors-proxy.herokuapp.com', {
+export const getSinglePlant = async (plantID) => {
+  const response = await fetch('https://fe-cors-proxy.herokuapp.com', {
     headers: {
       "Target-URL": `https://trefle.io/api/v1/species/${plantID}?token=${API_KEY}`
     }
   })
-  .then(response => response.json())
+  const data = await response.json()
+  return data
 }
