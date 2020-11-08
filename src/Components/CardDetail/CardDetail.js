fe8842684aa9ec841d80d73d73aa7bd0a8559d86
Stacy Potten <stacylynnaz@gmail.com>
2020-11-08 11:02:08 -0700

    Refactor to better access data from api

--- src/Components/CardDetail/CardDetail.js
+++ src/Components/CardDetail/CardDetail.js
@@ -6,20 +6,26 @@ class CardDetail extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      plant: {}
+      plantType: {}
     }
   }
 
-  componentDidMount() {
+  componentDidMount = () => {
     getSinglePlant(this.props.id)
-    .then(data => this.setState({plant: data.data}))
+    .then(data => this.setState({plantType: data.data.specifications}))
     .catch(error => console.log('single plant fetch error'))
   }
 
   render() {
     return (
       <main>
-        <h1>Hello</h1>
+        <h1>{this.props.common_name}</h1>
+        <h2>{this.props.scientific_name}</h2>
+        <h2>Native to Japan: </h2>
+        {this.state.plantType.growth_habit !== null && <h2>Plant Type: {this.state.plantType.growth_habit}</h2>}
+        <h2>Learn More: </h2>
+        <button className='add-button' type='button'>Add to Garden Plan</button>
+        <img className='photo' src={this.props.image_url} alt= {this.props.common_name}/>
       </main>
     )
   }
