318e9a0c0bb838ba5b63a1b857d9a8350bc1edda
Stacy Potten <stacylynnaz@gmail.com>
2020-11-09 11:11:48 -0700

    Add unit test for title on App

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import { BrowserRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  it('should render a title', () => {
+    const mockTitle = 'Sakura'
+
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    )
+
+    expect(screen.getByText('Sakura')).toBeInTheDocument();
+  })
+
+  
+})
--- src/Components/Card/Card.test.js
+++ src/Components/Card/Card.test.js
@@ -5,9 +5,6 @@ import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-
-//two things to test: render a book card, when card is clicked routes to detail view
-
 describe('Card', () => {
   it('should render a plant card', () => {
     const mockCard = {
@@ -18,17 +15,18 @@ describe('Card', () => {
 
     render(
       <BrowserRouter>
-      <Card
-        id={12345}
-        key={1}
-        commonName={mockCard.commonName}
-        scientificName={mockCard.scientficName}
-        image={mockCard.image}
-      />
+        <Card
+          id={12345}
+          key={1}
+          commonName={mockCard.commonName}
+          scientificName={mockCard.scientficName}
+          image={mockCard.image}
+        />
       </BrowserRouter>
     )
 
     expect(screen.getByText('Japanese cheesewood')).toBeInTheDocument()
     expect(screen.getByText('Pittosporum tobira')).toBeInTheDocument()
   })
+
 })
