f075d526fb2a8320cfc3c05c19492425ba237575
Stacy Potten <stacylynnaz@gmail.com>
2020-11-09 13:37:57 -0700

    Add test for remove button on plan component

--- src/Components/Plan/Plan.test.js
+++ src/Components/Plan/Plan.test.js
@@ -35,7 +35,6 @@ describe('Plan', () => {
   it('should display a message if there are no plants in the plan', () => {
     const fakePlantList = []
     const fakeRemove = jest.fn()
-    // const fakeMessage = 'Return to the home page and add some plants to plan your garden.'
 
     render (
       <BrowserRouter>
@@ -47,8 +46,42 @@ describe('Plan', () => {
 
     expect(screen.getByText('Return to the Home page and add some plants to plan your garden.')).toBeInTheDocument()
   })
-})
 
-//test render
-//test message
-//test button functionality-removeFromPlan
+  it('should remove a plant from the list on button click', () => {
+    const fakePlantList = [
+      {
+        id: 1234,
+        image: 'mock URL1',
+        commonName: 'Japanese cheesewood',
+        scientficName: 'Pittosporum tobira'
+      },
+      {
+        id: 4567,
+        image: 'mock URL2',
+        commonName: 'Japanese raspberry',
+        scientficName: 'Rubus parvifolius'
+      }
+    ]
+    const fakeRemove = jest.fn()
+
+    render (
+      <BrowserRouter>
+        <Plan plan={fakePlantList}
+        removeFromPlan={fakeRemove}
+        />
+      </BrowserRouter>
+    )
+
+    //what should render with 2 plants added to plan
+    expect(fakePlantList).toHaveLength(2);
+    expect(screen.getByText('Japanese cheesewood')).toBeInTheDocument();
+    expect(screen.getByText('Japanese raspberry')).toBeInTheDocument();
+
+    //buttons render and the first card's button is clicked
+    const removes = screen.getAllByText('Remove from Garden Plan')
+    expect(removes).toHaveLength(2)
+    userEvent.click(removes[0])
+    expect(fakeRemove).toHaveBeenCalledWith(1234)
+    expect(fakeRemove).toHaveBeenCalledTimes(1)
+  })
+})
