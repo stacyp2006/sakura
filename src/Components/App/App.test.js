ae5ef216b428f1035f63d0bb3974067ce975aaae
Stacy Potten <stacylynnaz@gmail.com>
2020-11-09 11:14:49 -0700

    Add unit test for link render

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -18,5 +18,17 @@ describe('App', () => {
     expect(screen.getByText('Sakura')).toBeInTheDocument();
   })
 
-  
+  it('should render links for Home and Plan pages', () => {
+    const mockHomeLink = 'Home'
+    const mockPlanLink = 'My Garden Plan'
+
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    )
+
+    expect(screen.getByText('Home')).toBeInTheDocument()
+    expect(screen.getByText('My Garden Plan')).toBeInTheDocument()
+  })
 })
